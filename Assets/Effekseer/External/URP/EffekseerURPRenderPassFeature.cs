517581ca9ac8750e4b8449d03e785543663607cf
durswd <swda.durl@gmail.com>
2022-12-27 20:11:53 +0900

    1.70b

--- Assets/Effekseer/External/URP/EffekseerURPRenderPassFeature.cs
+++ Assets/Effekseer/External/URP/EffekseerURPRenderPassFeature.cs
@@ -15,17 +15,24 @@ public class UrpBlitter : IEffekseerBlitter
 		this.blitMaterial = CoreUtils.CreateEngineMaterial("Hidden/Universal Render Pipeline/Blit");
 	}
 
-	public void Blit(CommandBuffer cmd, RenderTargetIdentifier source, RenderTargetIdentifier dest)
+	public void Blit(CommandBuffer cmd, RenderTargetIdentifier source, RenderTargetIdentifier dest, bool xrRendering)
 	{
-		CoreUtils.SetRenderTarget(
-			cmd,
-			dest,
-			RenderBufferLoadAction.Load,
-			RenderBufferStoreAction.Store,
-			ClearFlag.None,
-			Color.black);
-		cmd.SetGlobalTexture(sourceTex, source);
-		cmd.DrawProcedural(Matrix4x4.identity, blitMaterial, 0, MeshTopology.Quads, 4);
+		if(xrRendering)
+		{
+			CoreUtils.SetRenderTarget(
+				cmd,
+				dest,
+				RenderBufferLoadAction.Load,
+				RenderBufferStoreAction.Store,
+				ClearFlag.None,
+				Color.black);
+			cmd.SetGlobalTexture(sourceTex, source);
+			cmd.DrawProcedural(Matrix4x4.identity, blitMaterial, 0, MeshTopology.Quads, 4);
+		}
+		else
+		{
+			cmd.Blit(source, dest);
+		}
 	}
 }
 
@@ -45,16 +52,37 @@ public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
 			this.renderPassEvent = UnityEngine.Rendering.Universal.RenderPassEvent.AfterRenderingTransparents;
 		}
 
-		bool IsValid(RenderTargetIdentifier identifer)
+		bool IsValidCameraDepthTarget(RenderTargetIdentifier cameraDepthTarget)
 		{
-			// HACK
-			return !identifer.ToString().Contains("NameID -1");
+			// HACK: When using URP, the depth might be either written to a DepthBuffer attached to
+			//       - cameraColorTarget
+			//       OR
+			//       - cameraDepthTarget
+			//
+			//       Which one contains the depth is dependent on many variables including but not limited to:
+			//       - Unity Editor version
+			//       - whether camera stacking is used
+			//       - whether MSAA is enabled
+			//       - whether Depth Texture is enabled
+			//
+			//       Effekseer needs to know where it can access the depth buffer. This hack checks if the depth is
+			//       written to cameraDepthTarget based on the observation, that whenever Unity is writing depth to
+			//       cameraDepthTarget, cameraDepthTarget's RenderTargetIdentifier contains either
+			//       - NameId xxx (where xxx is an integer other than -1)
+			//       OR
+			//       - InstanceID yyy (where yyy is an integer other than 0)
+			//
+			//       A RenderTargetIdentifier might point to a valid RenderTexture in many different ways
+			//       (including NameID or InstanceID), whether NameID or InstanceID is used to identify a valid
+			//       RenderTexture depends on the Unity Editor / URP package version.
+			var identifierString = cameraDepthTarget.ToString();
+			return !identifierString.Contains("NameID -1") || !identifierString.Contains("InstanceID 0");
 		}
 
 #if !EFFEKSEER_URP_DEPTHTARGET_FIX
 		public void Setup(RenderTargetIdentifier cameraColorTarget, RenderTargetIdentifier cameraDepthTarget)
 		{
-			bool isValidDepth = IsValid(cameraDepthTarget);
+			bool isValidDepth = IsValidCameraDepthTarget(cameraDepthTarget);
 
 			this.cameraColorTarget = cameraColorTarget;
 			prop.colorTargetIdentifier = cameraColorTarget;
@@ -75,7 +103,10 @@ public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
 			var renderer = renderingData.cameraData.renderer;
 			prop.colorTargetIdentifier = renderer.cameraColorTarget;
 
-			var isValidDepth = IsValid(renderer.cameraDepthTarget);
+			// NOTE: We need to know whether the depth in cameraDepthTarget is valid or not since if it is valid,
+			//       we need to pass cameraDepthTarget to SetRenderTarget() later on. If it isn't valid, the depth in
+			//       cameraColorTarget is used instead.
+			var isValidDepth = IsValidCameraDepthTarget(renderer.cameraDepthTarget);
 
 			if (isValidDepth)
 			{
@@ -89,6 +120,9 @@ public class EffekseerURPRenderPassFeature : ScriptableRendererFeature
 			prop.colorTargetDescriptor = renderingData.cameraData.cameraTargetDescriptor;
 			prop.isRequiredToCopyBackground = true;
 			prop.renderFeature = Effekseer.Internal.RenderFeature.URP;
+#if EFFEKSEER_URP_XRRENDERING
+			prop.xrRendering = renderingData.cameraData.xrRendering;
+#endif
 			prop.canGrabDepth = renderingData.cameraData.requiresDepthTexture;
 			Effekseer.EffekseerSystem.Instance.renderer.Render(renderingData.cameraData.camera, prop, null, blitter);
 			var commandBuffer = Effekseer.EffekseerSystem.Instance.renderer.GetCameraCommandBuffer(renderingData.cameraData.camera);
